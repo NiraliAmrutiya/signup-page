b2ec00aae60751ba6afe086a39924150abad934b
NiraliAmrutiya <niraliamrutiya1996@gmail.com>
2023-08-22 15:20:11 -0400

    password matches confirm-password

--- src/SignupForm/SignupForm.js
+++ src/SignupForm/SignupForm.js
@@ -1,26 +1,43 @@
 import { useState } from "react";
 
 const SignupForm = () => {
-  const [enteredEmail, setenteredEmail] = useState("");
+  const [enteredEmail, setEnteredEmail] = useState("");
+  const [enteredPwd, setEnteredPwd] = useState("");
+  const [enteredConfirmPwd, setEnteredConfirmPwd] = useState("");
   const [validEmail, setValidEmail] = useState("");
   const [error, setError] = useState("");
 
   const handleEmailChange = (e) => {
-    setenteredEmail(e.target.value);
-    setValidEmail(enteredEmail.includes('@'));
+    setEnteredEmail(e.target.value);
+    setValidEmail(enteredEmail.includes("@"));
   };
 
-  const handleValidEmail = e => {
+  //handles valid email onblur
+  const handleValidEmail = () => {
     if (!validEmail) {
-        setError("Please enter valid email")
+      setError("Please enter valid email");
+    } else {
+      setError("Valid Email add");
     }
-    else{
-        setError("Valid Email add")
-    }
-  }
+  };
+
+
+  const handlePwdChange = (e) => {
+    setEnteredPwd(e.target.value);
+  };
+
+  const handleConfirmPwdChange = (e) => {
+    setEnteredConfirmPwd(e.target.value);
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault(); // it prevent default property i.e. here it prevent from submitting the form also without preventDefault if the form is submitted the page gets reload
+    //checks the password and confirm password matches
+    if (enteredPwd === enteredConfirmPwd) {
+      console.log("Password does match");
+    } else {
+      console.log("Password does not match");
+    }
   };
   return (
     <>
@@ -40,12 +57,22 @@ const SignupForm = () => {
         <div>
           <label>Password</label>
           <br />
-          <input type="password" name="password" />
+          <input
+            type="password"
+            name="password"
+            value={enteredPwd}
+            onChange={handlePwdChange}
+          />
         </div>
         <div>
           <label>Confirm Password</label>
           <br />
-          <input type="password" name="confirm-password" />
+          <input
+            type="password"
+            name="confirm-password"
+            value={enteredConfirmPwd}
+            onChange={handleConfirmPwdChange}
+          />
         </div>
         <button>Create Button</button>
       </form>
