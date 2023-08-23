45a4ca67d91d6e70206d1f8b72c4464f3db032b1
NiraliAmrutiya <niraliamrutiya1996@gmail.com>
2023-08-23 15:53:43 -0400

    added password criteria

---
+++ src/RegEx.js
@@ -0,0 +1,2 @@
+export const validPassword = new RegExp("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[*.!@$%^&]).{10,}$");
+   
--- src/SignupForm/SignupForm.js
+++ src/SignupForm/SignupForm.js
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import { validPassword } from "../RegEx.js";
+//password conditions
 
 const SignupForm = () => {
   const [enteredEmail, setEnteredEmail] = useState("");
@@ -6,6 +8,7 @@ const SignupForm = () => {
   const [enteredConfirmPwd, setEnteredConfirmPwd] = useState("");
   const [validEmail, setValidEmail] = useState("");
   const [error, setError] = useState("");
+  const [pwdError, setPwdError] = useState("");
 
   const handleEmailChange = (e) => {
     setEnteredEmail(e.target.value);
@@ -21,6 +24,12 @@ const SignupForm = () => {
     }
   };
 
+  const handleValidPassword = () => {
+    console.log(enteredPwd);
+    if (!validPassword.test(enteredPwd)) {
+      setPwdError("Password is not valid");
+    }
+  };
 
   const handlePwdChange = (e) => {
     setEnteredPwd(e.target.value);
@@ -31,52 +40,54 @@ const SignupForm = () => {
   };
 
   const handleSubmit = (e) => {
-    e.preventDefault(); // it prevent default property i.e. here it prevent from submitting the form also without preventDefault if the form is submitted the page gets reload
+    // it prevent default property i.e. here it prevent from submitting the form also without preventDefault if the form is submitted the page gets reload
+    e.preventDefault();
+
     //checks the password and confirm password matches
     if (enteredPwd === enteredConfirmPwd) {
-      console.log("Password does match");
+      console.log("Confirm Password does match");
     } else {
       console.log("Password does not match");
     }
   };
   return (
-    <>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>Email</label>
-          <br />
-          <input
-            type="email"
-            name="email"
-            value={enteredEmail}
-            onChange={handleEmailChange}
-            onBlur={handleValidEmail}
-          />
-          <p>{error}</p>
-        </div>
-        <div>
-          <label>Password</label>
-          <br />
-          <input
-            type="password"
-            name="password"
-            value={enteredPwd}
-            onChange={handlePwdChange}
-          />
-        </div>
-        <div>
-          <label>Confirm Password</label>
-          <br />
-          <input
-            type="password"
-            name="confirm-password"
-            value={enteredConfirmPwd}
-            onChange={handleConfirmPwdChange}
-          />
-        </div>
-        <button>Create Button</button>
-      </form>
-    </>
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label>Email</label>
+        <br />
+        <input
+          type="email"
+          name="email"
+          value={enteredEmail}
+          onChange={handleEmailChange}
+          onBlur={handleValidEmail}
+        />
+        <p>{error}</p>
+      </div>
+      <div>
+        <label>Password</label>
+        <br />
+        <input
+          type="password"
+          name="password"
+          value={enteredPwd}
+          onChange={handlePwdChange}
+          onBlur={handleValidPassword}
+        />
+      </div>
+      <div>
+        <label>Confirm Password</label>
+        <br />
+        <input
+          type="password"
+          name="confirm-password"
+          value={enteredConfirmPwd}
+          onChange={handleConfirmPwdChange}
+        />
+        <p>{pwdError}</p>
+      </div>
+      <button>Create Button</button>
+    </form>
   );
 };
 
